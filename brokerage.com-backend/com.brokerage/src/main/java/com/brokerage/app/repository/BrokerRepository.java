300f72ee77ab72476dac787414f40c8b0aae8f6a
Aniket2108 <aniketlalit09@gmail.com>
2024-02-15 00:56:32 +0530

    New Frontend

--- brokerage.com-backend/com.brokerage/src/main/java/com/brokerage/app/repository/BrokerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brokerage.app.repository;
-
-import com.brokerage.app.entities.Broker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrokerRepository extends JpaRepository<Broker,Long> {
-}
