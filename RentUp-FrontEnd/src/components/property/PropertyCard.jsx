be538867968bfaed146950d820e54faa302429a1
Aniket2108 <aniketlalit09@gmail.com>
2024-02-17 19:14:26 +0530

    Updated FrontEnd + BackEnd

--- RentUp-FrontEnd/src/components/property/PropertyCard.jsx
+++ RentUp-FrontEnd/src/components/property/PropertyCard.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import "./PropertyCard.css";
 
 const PropertyCard = ({ property }) => {
+  // console.log(property);
   return (
     <div className="property-card">
       <h3 className="property-title">{property.address}</h3>
-      <p className="property-info">Property ID: {property.propertyId}</p>
-      <p className="property-info">Carpet Area: {property.carpetArea} sq. ft.</p>
-      <p className="property-info">Status: {property.status}</p>
+      <p className="property-info">City: {property.areaId.city.cityName}</p>
+      <p className="property-info">Area: {property.areaId.areaName}</p>
+      {/* <p className="property-info">Property ID: {property.propertyId}</p> */}
+      <p className="property-info">
+        Carpet Area: {property.carpetArea} sq. ft.
+      </p>
+      {/* <p className="property-info">Status: {property.status}</p> */}
       <p className="property-info">Tenant Type: {property.tenantType}</p>
       <p className="property-info">Flat Type: {property.flatType}</p>
+      <p className="property-info">Price: {property.price}</p>
 
       <div className="property-images">
         {property.propertyImages.map((image, index) => (
